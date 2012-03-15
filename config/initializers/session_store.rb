3a3243109d5dabacb10683d9ab6dd8f241399d26
Aidan Feldman <aidan.feldman@gmail.com>
2012-03-15 05:39:44 -0400

    remove all Rails files from master branch

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-RailsBottomUp::Application.config.session_store :cookie_store, key: '_rails-bottom-up_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# RailsBottomUp::Application.config.session_store :active_record_store
