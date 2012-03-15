ca7525eec555018419f4642d811615180bb01c5c
Aidan Feldman <aidan.feldman@gmail.com>
2012-03-15 05:02:12 -0400

    added proc as route

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,8 @@ RailsBottomUp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  # just a Rack app!
+  match '/hello' => lambda { |env| [200, {}, ["Hello World!"]] }
+
 end
