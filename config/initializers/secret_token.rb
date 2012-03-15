cf9455a0d9cd3f42dc9bacb2ab728cc9e326e597
Aidan Feldman <aidan.feldman@gmail.com>
2012-03-15 03:35:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBottomUp::Application.config.secret_token = '7b0a36174502075fd9272663dcc368e70e4e2384d04fb4e4ad1b0ff4c34c8d77d1d572bc433709e4a5f03e530079df761a32ccc05b59e07bc76003ac1266d1b9'
